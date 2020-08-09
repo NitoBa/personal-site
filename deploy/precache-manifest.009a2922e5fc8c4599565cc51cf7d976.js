dee0032aac614be199e9ed636ae229fc98c2c3e0
Bruno Alves <brukum2@gmail.com>
2020-08-09 01:26:02 -0300

    deploy site

---
+++ deploy/precache-manifest.009a2922e5fc8c4599565cc51cf7d976.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2d8040b5951e7094a5ff4133bc3819",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f010c10effed6a16cd",
+    "url": "/static/js/2.e5a90931.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5a90931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b86de2bf1f4c345d803e",
+    "url": "/static/js/main.a98bd974.chunk.js"
+  },
+  {
+    "revision": "7d4a7932cfdcd47c370c",
+    "url": "/static/js/runtime-main.0128b0c7.js"
+  },
+  {
+    "revision": "e24cd68e82830b6e3f1bd0e14b44170b",
+    "url": "/static/media/ilustration.e24cd68e.svg"
+  },
+  {
+    "revision": "31668303d88e5345d1406c1f3102ffd2",
+    "url": "/static/media/perfil3.31668303.png"
+  },
+  {
+    "revision": "59a882d51509866fe1a189db16abe7f9",
+    "url": "/static/media/perfilgithub.59a882d5.jpg"
+  }
+]);
